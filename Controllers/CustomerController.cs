af23f18d5a5a547e6324f53281d9b2d2d65867d2
Jeslur Rahman <88666503+it20667696@users.noreply.github.com>
2024-07-07 17:13:34 +0530

    Basic setup of CRUD operation without connecting DB

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,72 @@
+﻿using BasicDotNetCoreAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicDotNetCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public static List<Customer> customers = new()
+        {
+            new Customer { Id = 1, Name="Jeslur", City="Colombo", Country="Sri Lanaka"},
+            new Customer { Id = 1, Name="xxxx", City="Jaffna", Country="Sri Lanaka"},
+            new Customer { Id = 1, Name="yyyy", City="Matara", Country="Sri Lanaka"}
+
+        };
+
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCustomerById(int id)
+        {
+            var customer = customers.Find(c => c.Id == id);
+            if(customer == null)
+            {
+                return NotFound("Customer not found");
+            }
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomer(Customer customer)
+        {
+            customers.Add(customer);
+            return Ok(customers);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCustomer(Customer customer) 
+        {
+            var customerbyid = customers.Find(c => c.Id ==  customer.Id);
+            
+            if(customerbyid == null)
+            {
+                return NotFound("Invalid Customer details");
+            }
+            customerbyid.Name = customer.Name;
+            customerbyid.City = customer.City;
+            customerbyid.Country = customer.Country;
+
+            return Ok(customerbyid);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCustomer(int id) 
+        {
+            var customer = customers.Find(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound("Invalid Customer details");
+            }
+            customers.Remove(customer);
+            return Ok(customers);
+
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicDotNetCoreAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
